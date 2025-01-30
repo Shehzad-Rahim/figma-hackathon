56b6af42be3ed10d328d3789326d4610ac82cc76
Shehzad-Rahim <shehzadrahim33@gmail.com>
2025-01-30 20:16:43 +0500

    config change

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
-    experimental: {
-      appDir: true, // App Router enabled
-    },
     images: {
         domains: ["cdn.sanity.io"]
     },
