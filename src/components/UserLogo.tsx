762400e4f9bf4ed54e2bd8fd91204900075dcd91
Shehzad-Rahim <shehzadrahim33@gmail.com>
2025-02-15 00:56:39 +0500

    user logo adjusted

--- src/components/UserLogo.tsx
+++ src/components/UserLogo.tsx
@@ -4,12 +4,13 @@ import {
     SignedOut,
     UserButton
   } from '@clerk/nextjs'
-
 function UserLogo() {
   return (
     <>
         <SignedOut>
+          <div className="flex flex-col items-center w-[50px]">
             <SignInButton />
+          </div>
           </SignedOut>
           <SignedIn>
             <UserButton />
